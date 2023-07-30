db57da861f6da0191f6118b355bc6d9d31a88779
HadiJF03 <48181301+HadiJF03@users.noreply.github.com>
2023-07-30 06:56:47 +0300

    add password validation

--- script.js
+++ script.js
@@ -1,8 +1,11 @@
 const submit = document.querySelector(".create");
+const myForm = document.querySelector("form");
 submit.addEventListener("click",(e)=>{
     const pass = document.querySelector("input[name=pass]");
-    const confirmPass = document.querySelector("input[name=]");
-
-    console.log(pass.textContent);
-    console.log(confirmPass.textContent);
+    const confirmPass = document.querySelector("input[name=confirm-pass]");
+    if(!(pass.value===confirmPass.value)){
+        pass.classList.add("error");
+        confirmPass.classList.add("error");
+        alert("passwords must match")
+    }
 })

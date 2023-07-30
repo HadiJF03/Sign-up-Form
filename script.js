070fbf212381dcbb5e97d9cfdbb7159d0ba1363c
HadiJF03 <48181301+HadiJF03@users.noreply.github.com>
2023-07-30 06:23:44 +0300

    added sumbit button and polished css

---
+++ script.js
@@ -0,0 +1,8 @@
+const submit = document.querySelector(".create");
+submit.addEventListener("click",(e)=>{
+    const pass = document.querySelector("input[name=pass]");
+    const confirmPass = document.querySelector("input[name=]");
+
+    console.log(pass.textContent);
+    console.log(confirmPass.textContent);
+})
